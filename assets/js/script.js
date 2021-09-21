6137814a6605fc5303f2013ea2ae23f05e7f27df
AS <asadrgilany7@gmail.com>
2021-09-20 22:43:50 -0400

    changed js to add another fetch request with the city's lat/lon in order to get uvi

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var cityInputEl = $("#city-input");
 var weatherContainerEl = $("#weather-snapshot");
 var cityNameHeader = $("#city-header");
 var cityDescription = $("#city-description");
+var iconImgEl = $("#weather-icon");
+var currentCity = "";
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -19,15 +21,31 @@ var formSubmitHandler = function(event) {
 }
 
 var getCityWeather = function(city) {
-    var apiKey = "5aedbab5c11e388615ce282aa37ce509";
+    var apiKey = "&appid=5aedbab5c11e388615ce282aa37ce509";
     var unitType = "&units=imperial";
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + unitType + "&appid=" + apiKey;
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + unitType + apiKey;
 
     fetch(apiUrl)
         .then(function(response) {
             if (response.ok) {
                 response.json().then(function(data) {
-                    displayCityWeather(data, city);
+                    currentCity = data.name;
+                    var cityLat = "lat=" + data.coord.lat;
+                    var cityLon = "lon=" + data.coord.lon;
+                    var exclusions = "&exclude=minutely,hourly";
+                    var newApiUrl = "https://api.openweathermap.org/data/2.5/onecall?" + cityLat + "&" + cityLon + exclusions + unitType + apiKey;
+                    fetch(newApiUrl).then(function(response) {
+                        if (response.ok) {
+                            response.json().then(function(data2) {
+                                console.log(data2);
+                                displayCityWeather(data2, city);
+                            })
+                        }
+                        else {
+                            alert("Error: City Not Found");
+                        }
+                    })
+                    
                 });
             }
             else {
@@ -39,38 +57,44 @@ var getCityWeather = function(city) {
         });
 }
 
-var displayCityWeather = function(weatherData) {
+var displayCityWeather = function(cityData) {
 
     weatherContainerEl.text("");
 
-    var weatherLocation = weatherData.name;
     var todayDate = moment().format("L");
-    var weatherIcon = weatherData.weather[0].icon;
-    var iconUrl = "https://openweathermap.org/img/w/" + weatherIcon + ".png";
-    var weatherDescription = weatherData.weather[0].description;
-
-    cityNameHeader.html(weatherLocation + " " + todayDate + " <img src='" + iconUrl + "' alt='Weather Icon'/> " + weatherDescription);
-
-    cityDescription.text(weatherDescription);
+    // Take the info we want from the data and set to new variables
+    var weatherTemp = cityData.current.temp + " °F";
+    var weatherHumidity = cityData.current.humidity + " %";
+    var weatherWind = cityData.current.wind_speed + " mph";
+    var weatherUvi = cityData.current.uvi;
+    var weatherIconCode = cityData.current.weather[0].icon;
+    var weatherDescription = cityData.current.weather[0].description;
     
-    var weatherTemp = weatherData.main.temp;
-    var weatherWind = weatherData.wind.speed;
-    var weatherHumidity = weatherData.main.humidity;
+    var iconUrl = "https://openweathermap.org/img/w/" + weatherIconCode + ".png";
+
+    cityNameHeader.parent().attr("id", "weather-results");
+
+    cityNameHeader.html(currentCity + " " + todayDate + " <img src='" + iconUrl + "' alt='Weather Icon'/> " + weatherDescription);
 
     var newWeatherObject = {
-        Temperature: weatherTemp + "°F",
-        Wind: weatherWind + "mph",
-        Humidity: weatherHumidity + "%"
+        Temperature: weatherTemp,
+        Humidity: weatherHumidity,
+        Wind: weatherWind,
+        UVIndex: weatherUvi 
     }
 
     for (var key in newWeatherObject) {
         if (newWeatherObject.hasOwnProperty(key)) {
-            var pEl = $("<p></p>");
+            var listItem = $("<li></li>");
 
-            pEl.text(key + ": " + newWeatherObject[key]);
-
-            weatherContainerEl.append(pEl);
-
+            if (newWeatherObject[key] === weatherUvi) {
+                listItem.html(key + ": " + "<span id='uvi-color'>" + newWeatherObject[key] + "</span>");
+            }
+            else {
+                listItem.text(key + ": " + newWeatherObject[key]);
+            }
+        
+            weatherContainerEl.append(listItem);
         }
     }
 
